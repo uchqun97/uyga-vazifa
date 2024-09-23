726eb46f1ca7258329a22411b59361f0eff4e75c
uchqun97 <uchqun1997shoh@gmail.com>
2024-09-24 00:17:25 +0500

    first commit

---
+++ 11.js
@@ -0,0 +1,107 @@
+
+// const a=[2, 20, 15]
+// function addOneToEachElement(){
+// a.forEach((item)=>{
+//   console.log(item*2)
+//   })
+
+// }
+
+
+  
+// addOneToEachElement()
+
+// let a = [2, 3, 2];
+// let b = [4, -2, 3];
+// let c = [6, -8, 1];
+
+
+// function addTwoArrays() {
+//     a.forEach((item, index) => {
+//         console.log(item + b[index]+c[index]);
+//     });
+// }addTwoArrays()
+
+
+// const fruits = ["Banan", "olma", "gilos", "uzum"];
+// let length = fruits.length;
+// console.log(fruits.length)
+
+
+// const fruits = ["ruchka", "daftar", "sumka"];
+// fruits.push("qalam");
+// console.log(fruits)
+
+
+
+
+// let numbers = [11, 31, 22, 41, 15,16];
+// numbers.splice(4, 2);
+
+// console.log(numbers);
+
+
+
+
+
+// const svetafor = ['qizil', 'sariq', 'yashil'];
+
+// svetafor.forEach(rang => {
+//     const userInput = prompt(`Svetafor rangini kiriting: ${svetafor.join(', ')}`);
+
+//     if (userInput === 'qizil') {
+//         alert('Tinchtur!');
+//     } else if (userInput === 'sariq') {
+//         alert('Tayyorlan!');
+//     } else if (userInput === 'yashil') {
+//         alert('Tez yur!');
+//     } else {
+//         alert('svetaforda bunaqa rang yoq');
+//     }
+// });
+     
+
+
+
+
+//Oxshamadi  shu kod
+// const a = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// const b = [
+//     [3, 2, 3],
+//     [4, 6, 6],
+//     [7, 4, 9]
+// ];
+
+// function addTwoArrays() {
+//          a.forEach((item, index) => {
+//             console.log(item + b[index]);
+//        });
+//     }addTwoArrays()
+
+
+
+// const a=[4, 16, 25]
+// function addOneToEachElement(){
+// a.forEach((item)=>{
+//     console.log(Math.sqrt(item))
+//   })
+
+// }addOneToEachElement();
+
+
+
+
+// const a = [1, 2, 3, 4, 5, 6,7,8,9,10,1,11,12,1111,18];
+// let count = 0;
+
+// a.forEach((item) => {
+//     if (item % 2 === 0 && item % 3 === 0) {
+//         count++;
+//     }
+// });
+
+// console.log(`Massivda 2 ga va 3 ga ham bo'linadigan sonlar soni: ${count}`);
